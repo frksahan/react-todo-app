62a23dc049ceae7b489ef9b8e900321277dda6eb
Ömer Faruk <sahan.faruk.omer@outlook.com>
2023-03-26 23:22:24 +0300

    start todo app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+
+
+function App() {
+  return (
+    <div className='todoapp'>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
