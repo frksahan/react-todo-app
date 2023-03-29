26f5588af31116b80e33e5c2d73308e5293df399
Ömer Faruk <sahan.faruk.omer@outlook.com>
2023-03-29 23:33:22 +0300

    continue todoapp

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 
-const [todo, setTodo] = useState("");
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import React, { useState } from 'react'
 
-const [todo, setTodo] = useState("");
 
 function Header() {
     return (
