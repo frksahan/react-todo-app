594dc709fefe1b1277d0a20d6081469f156b057b
Ömer Faruk <sahan.faruk.omer@outlook.com>
2023-03-30 23:55:45 +0300

    some bug have

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from "react";
 
-function Footer() {
+const Header = ({ InputText, setInputText, todos, setTodos }) => {
+    // Propsları burada alıyoruz.
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    }; //
+    const submitTodoHandler = (e) => {
+        e.preventDefault(); // sayfa yenilenmesini engelliyoruz.
+        if (InputText === "") {
+            alert("Please enter a todo");
+            return; // input boşsa fonksiyonu sonlandırıyoruz.
+        }
+        setTodos([
+            ...todos, // önceki todoları koruyoruz.
+            { text: InputText, completed: false, id: Math.random() * 1000 }, // Burada obje oluşturuyoruz.
+        ]);
+        setInputText(""); // inputu temizliyoruz.
+    };
     return (
-        <footer class="footer">
-            <span class="todo-count">
-                <strong>2</strong>
-                items left
-            </span>
+        <div>
+            <header className="header">
+                <h1>todos</h1>
+                <form onSubmit={submitTodoHandler}>
+                    <input
+                        className="new-todo"
+                        placeholder="What needs to be done?"
+                        autoFocus
+                        value={InputText}
+                        onChange={inputTextHandler}
+                    />
+                </form>
+            </header>
+        </div>
+    );
+};
 
-            <ul class="filters">
-                <li>
-                    <a href="#/" class="selected">All</a>
-                </li>
-                <li>
-                    <a href="#/">Active</a>
-                </li>
-                <li>
-                    <a href="#/">Completed</a>
-                </li>
-            </ul>
-
-            <button class="clear-completed">
-                Clear completed
-            </button>
-        </footer>
-    )
-}
-
-export default Footer
+export default Header;
